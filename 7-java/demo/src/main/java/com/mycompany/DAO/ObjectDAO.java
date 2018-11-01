2cbc6d4f738637a9a6ea5ec643ec53c72e0aa6eb
kviktorman <k.viktorman@yahoo.co.uk>
2018-11-01 14:29:15 +0100

    add java example

---
+++ 7-java/demo/src/main/java/com/mycompany/DAO/ObjectDAO.java
@@ -0,0 +1,11 @@
+package com.mycompany.DAO;
+
+public class ObjectDAO {
+    public String  myVariable = "test";
+    
+    
+    public String DoSomething(){
+    return "This is the return string";
+    
+    }
+}
---
+++ 7-java/demo/src/main/java/com/mycompany/Objects/ObjectDemo.java
@@ -0,0 +1,11 @@
+package com.mycompany.Objects;
+
+import com.mycompany.DAO.ObjectDAO;
+
+public class ObjectDemo {
+
+    public ObjectDemo(){
+        
+        ObjectDAO objectDAO = new ObjectDAO();
+    }
+}
